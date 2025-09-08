eb4dda3c8f839719e9982adeee6b94ba05b1d1eb
JVmendonca <craftgame157@gmail.com>
2025-09-08 17:33:44 -0300

    fix: requeriments

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn E-Comerce.wsgi
--- loja/settings.py
+++ loja/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ki5fn*x%)#5527@!b#r4y0^1cjs-)fsc^nbkik@8vf0j&8-r44
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
